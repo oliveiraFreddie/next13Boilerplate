98bc6967e616a1bc21850270837721ae8804c515
oliveiraFreddie <freddie.oliveira95@gmail.com>
2023-09-06 09:55:23 +0100

    Add gitworkflows

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,14 +4,23 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    //renderiza o component
+    // renderiza o component
     const { container } = render(<Main />)
-    //busca o elemento e verifica a existencia dele
+
+    // busca o elemento e verifica a existência dele
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
-    //gerar snapshot
+    // gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
